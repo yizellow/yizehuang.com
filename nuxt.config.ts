461f9157ef6ef047c4edfbdb84a5102ddf5dae24
Yizeh Huang <yizehuang@nextcapital.global>
2025-05-08 18:40:18 +0800

    01

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/app.css"],
+});
