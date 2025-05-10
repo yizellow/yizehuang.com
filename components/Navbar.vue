aac3f8ea40c832652067ceae9f5a0e1c60aa8d80
Yizeh Huang <yizehuang@nextcapital.global>
2025-05-11 01:13:47 +0800

    hover-frame

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -17,19 +17,65 @@ onMounted(() => {
     { opacity: 0 },
     { opacity: 1, ease: "steps(10)", duration: 0.8 }
   );
+  gsap.set(frame, { borderRadius: "40px", width: "40%" });
 
   frame.addEventListener("mouseenter", () => {
-    gsap.to(frame, { ogpacity: 0.2, duration: 0.3 }); // 降低透明度
+    gsap.to(frame, {
+      borderRadius: "0px",
+      width: "80%",
+      ease: "power2.out",
+      duration: 0.2,
+    });
+  
+
+
   });
 
-  // 滑鼠移出 → 淡入
   frame.addEventListener("mouseleave", () => {
-    gsap.to(frame, { opacity: 1, duration: 0.8 }); // 回來原本透明度
+    gsap.to(frame, {
+      borderRadius: "40px",
+      width: "40%",
+      ease: "power2.out",
+      duration: 0.2,
+    });
   });
 });
 </script>
+
 <template>
   <div class="flex justify-center items-center">
+    <div
+      ref="frameRef"
+      class="navbar-frame w-4/10 flex flex-col justify-center items-center p-2 mt-2 min-h-14 border-1 border-secondary shadow-md"
+    >
+      <div class="flex w-full items-center justify-center">
+        <Icon
+          icon="material-symbols:other-houses"
+          class="navbar-icon flex-1 px-2 text-center text-large sm:text-xl"
+        />
+
+        <Icon
+          icon="material-symbols:background-grid-small-sharp"
+          class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
+        />
+
+        <Icon
+          icon="material-symbols:ar-stickers-sharp"
+          class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
+        />
+        <Icon
+          icon="material-symbols:person"
+          class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
+        />
+        <Icon
+          icon="material-symbols:coffee-sharp"
+          class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
+        />
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="flex justify-center items-center">
     <div
       class="w-11/12 rounded-sm flex flex-col justify-center items-center p-2 mt-2 min-h-10 md:min-h-14 border-1 border-secondary shadow-md"
     >
@@ -61,9 +107,9 @@ onMounted(() => {
         >
       </div>
     </div>
-  </div>
+  </div> -->
 
-  <div class="flex justify-center items-center">
+  <!-- <div class="flex justify-center items-center">
     <div
       ref="frameRef"
       class="navbar-frame w-10/12 md:w-6/10 rounded-full flex flex-col justify-center items-center p-2 mt-2 min-h-8 sm:min-h-14 border-1 border-secondary shadow-md"
@@ -93,6 +139,6 @@ onMounted(() => {
         />
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 <style scoped></style>
