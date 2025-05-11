25daef28c5f5cb76ceca042950f05b2af4df1500
Yizeh Huang <yizehuang@nextcapital.global>
2025-05-11 16:29:52 +0800

    icon turn into text

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,19 @@
 <script setup>
 import { gsap } from "gsap";
 import { Icon } from "@iconify/vue";
+import { ref, onMounted } from "vue";
 
 const frameRef = ref(null);
+const showText = ref(false);
+
+const icons = [
+  "material-symbols:other-houses",
+  "material-symbols:background-grid-small-sharp",
+  "material-symbols:ar-stickers-sharp",
+  "material-symbols:person",
+  "material-symbols:coffee-sharp",
+];
+const menuItems = ["HOME", "PROJECTS", "ABOUT", "MEMBER", "DONATE"];
 
 onMounted(() => {
   const frame = frameRef.value;
@@ -18,59 +29,55 @@ onMounted(() => {
     { opacity: 1, ease: "steps(10)", duration: 0.8 }
   );
   gsap.set(frame, { borderRadius: "40px", width: "40%" });
-
-  frame.addEventListener("mouseenter", () => {
-    gsap.to(frame, {
-      borderRadius: "0px",
-      width: "80%",
-      ease: "power2.out",
-      duration: 0.2,
-    });
-  
-
-
-  });
-
-  frame.addEventListener("mouseleave", () => {
-    gsap.to(frame, {
-      borderRadius: "40px",
-      width: "40%",
-      ease: "power2.out",
-      duration: 0.2,
-    });
-  });
 });
+
+function onEnter() {
+  showText.value = true;
+  gsap.to(frameRef.value, {
+    borderRadius: "0px",
+    width: "80%",
+    ease: "back.out(1.7)",
+    duration: 0.5,
+  });
+}
+
+function onLeave() {
+  showText.value = false;
+  gsap.to(frameRef.value, {
+    borderRadius: "40px",
+    width: "40%",
+    ease: "back.out(1.7)",
+    duration: 0.5,
+  });
+}
 </script>
 
 <template>
   <div class="flex justify-center items-center">
     <div
       ref="frameRef"
-      class="navbar-frame w-4/10 flex flex-col justify-center items-center p-2 mt-2 min-h-14 border-1 border-secondary shadow-md"
+      class="navbar-frame flex flex-col justify-center items-center p-2 mt-2 min-h-14 border-1 border-secondary shadow-md"
+      @mouseenter="onEnter"
+      @mouseleave="onLeave"
     >
       <div class="flex w-full items-center justify-center">
-        <Icon
-          icon="material-symbols:other-houses"
-          class="navbar-icon flex-1 px-2 text-center text-large sm:text-xl"
-        />
-
-        <Icon
-          icon="material-symbols:background-grid-small-sharp"
-          class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
-        />
-
-        <Icon
-          icon="material-symbols:ar-stickers-sharp"
-          class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
-        />
-        <Icon
-          icon="material-symbols:person"
-          class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
-        />
-        <Icon
-          icon="material-symbols:coffee-sharp"
-          class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
-        />
+        <template v-if="showText">
+          <a
+            v-for="item in menuItems"
+            :key="item"
+            class="hover:underline flex-1 px-1 text-center text-xs sm:text-base md:text-lg"
+            href="#"
+            >{{ item }}</a
+          >
+        </template>
+        <template v-else>
+          <Icon
+            v-for="icon in icons"
+            :key="icon"
+            :icon="icon"
+            class="navbar-icon flex-1 px-2 text-center text-xl sm:text-2xl"
+          />
+        </template>
       </div>
     </div>
   </div>
