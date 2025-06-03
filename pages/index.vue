9837c882301c33c855cc0b97e14da16a3318eb9e
Yizeh Huang <yizehuang@nextcapital.global>
2025-06-03 10:52:18 +0800

    tunnel donw

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import LandingPage from "~/components/three.js/LandingPage.vue";
+// import LandingPage from "~/components/three.js/LandingPage.vue";
+import Particle from "~/components/three.js/Particle.vue";
 </script>
 
 <template>
   <NavbarDesktop />
   <!-- <NavbarMobile /> -->
-  <LandingPage />
+  <!-- <LandingPage /> -->
+  <Particle />
+
   <main></main>
 </template>
