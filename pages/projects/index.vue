5229276f0d92dac6c87e8a92a73bd54ebc58dbd5
yizellow <b10821056@gmcc.tnua.edu.tw>
2025-07-02 17:17:46 +0800

    project page basic done

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Carousel3D from "~/components/global/Carousel3DCarousel3D.vue";
+import Carousel3D from "~/components/global/Carousel3D.vue";
 
 definePageMeta({
   layout: "default",
@@ -8,10 +8,5 @@ definePageMeta({
 </script>
 
 <template>
-  <Carousel3D
-    :items="[
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.doggy-lab.com%2Fblog%2Fpuppy-socialization&psig=AOvVaw1srM3vwrAZCNcgZpj8_9-T&ust=1751448349088000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj5_tarm44DFQAAAAAdAAAAABAE',
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.doggy-lab.com%2Fblog%2Fpuppy-socialization&psig=AOvVaw1srM3vwrAZCNcgZpj8_9-T&ust=1751448349088000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPj5_tarm44DFQAAAAAdAAAAABAE',
-    ]"
-  />
+  <Carousel3D />
 </template>
