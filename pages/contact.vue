b03f07d715784f0b1f2acb26307633f0b30b81eb
yizellow <b10821056@gmcc.tnua.edu.tw>
2025-07-09 18:47:17 +0800

    video and card

--- pages/contact.vue
+++ pages/contact.vue
@@ -6,7 +6,7 @@
 
 import { ref, onMounted } from "vue";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,6 +15,8 @@ const card = ref(null);
 onMounted(() => {
   // 初始位置與 transform 設定，確保一開始置中
   gsap.set(card.value, {
+    width: "5vw",
+    height: "10vw",
     xPercent: -50,
     yPercent: -50,
     left: "50vw",
@@ -39,14 +41,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="relative w-screen h-[200vh] bg-black">
-    <!-- Sticky 區域 -->
-    <section class="sticky h-screen top-0 bg-green-200">
+  <div class="relative w-screen h-[200vh]">
+    <section class="sticky h-screen top-0 bg-green-100">
       <!-- 中間的卡片 -->
-      <div
-        ref="card"
-        class="bg-red-500 h-[50vh] w-[20vw] z-10 rounded-xl"
-      ></div>
+      <video autoplay loop muted playsinline class="w-sceen h-screen z-5">
+        <source src="/videos/yizehuang.mp4" type="video/mp4" />
+      </video>
+
+      <div ref="card" class="bg-red-500 z-10 rounded-xl"></div>
     </section>
   </div>
 </template>
