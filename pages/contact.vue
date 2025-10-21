924c51e045179abd713ff5f4e46acef10cb78751
Yize Huang <yizehuang@sysadmins-Mac-mini-2.local>
2025-10-21 14:09:05 +0200

    contact and exhibiton and rn try to fix x bar again

--- pages/contact.vue
+++ pages/contact.vue
@@ -117,13 +117,23 @@ onMounted(() => {
   </div>
 
   <section class="relative w-screen h-[100vh] bg-red-500">
-    <div class="w-full h-full">
+    <div class="w-full h-full border-amber-100 border-2">
       <client-only>
         <Gallery />
       </client-only>
-      <div class="w-full h-[10vh] bg-secondary z-20">ssss</div>
+      <div
+        class="w-full h-[70px] z-20 flex justify-center items-start bg-amber-100"
+      >
+        <div
+          class="bg-secondary w-[100px] h-4/5 border-2 border-secondary text-white"
+        >
+          ssss
+        </div>
+      </div>
 
-      <section class="w-full h-full flex justify-center items-center">
+      <section
+        class="w-full h-[50vh] flex justify-center items-start border-2 border-amber-300"
+      >
         <div class="w-1/2 h-auto p-4 border-2 border-primary">
           <div
             v-for="(exhibit, index) in exhibitions"
@@ -131,20 +141,20 @@ onMounted(() => {
             class="mb-5"
           >
             <p
-              class="flex justify-between items-center m-0 text-primary text-xs bg-secondary"
+              class="flex justify-between items-center m-0 text-primary text-xs"
             >
               <!-- 左邊：時間 + 名稱 -->
               <span class="flex gap-2 items-center">
-                <span class="newsreader text-xs text-white">{{
+                <span class="newsreader text-xs text-secondary">{{
                   exhibit.date
                 }}</span>
-                <h1 class="silkscreen text-base text-white">
+                <h1 class="silkscreen text-base text-secondary">
                   {{ exhibit.title }}
                 </h1>
               </span>
 
               <!-- 右邊：地點 -->
-              <span class="newsreader text-xs text-right text-white">{{
+              <span class="newsreader text-xs text-right text-secondary">{{
                 exhibit.location
               }}</span>
             </p>
@@ -152,7 +162,19 @@ onMounted(() => {
         </div>
       </section>
 
-      <footer class="w-full h-[20vh] flex flex-col bg-primary"></footer>
+      <footer
+        class="w-full h-[7vh] flex boreder text-secondary justify-center items-center border-2 border-dashed"
+      >
+        <div
+          class="newsreader flex flex-row items-center justify-center gap-4 w-7/8 h-full"
+        >
+          <p>Email: yize0926@gmail.com</p>
+          <p>whatapp: 886+981488850</p>
+          <p>instagram: _yi_ze_</p>
+        </div>
+      </footer>
     </div>
   </section>
 </template>
+
+<style scoped></style>
