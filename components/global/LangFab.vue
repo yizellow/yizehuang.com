24b08a7c9ab356ecfb845529ef3c9e58d792c1f8
yizellow <b10821056@gmcc.tnua.edu.tw>
2025-12-29 18:03:09 +0100

    i18n done

---
+++ components/global/LangFab.vue
@@ -0,0 +1,33 @@
+<!-- components/LangFab.vue -->
+<script setup lang="ts">
+const switchLocalePath = useSwitchLocalePath();
+const { locale } = useI18n();
+</script>
+
+<template>
+  <div class="fixed bottom-6 right-6 z-50 flex items-center gap-2">
+    <NuxtLink
+      :to="switchLocalePath('en')"
+      class="rounded-full border px-3 py-2 text-xs backdrop-blur"
+      :class="
+        locale === 'en'
+          ? 'bg-secondary/40 text-primary'
+          : 'bg-white text-primary'
+      "
+    >
+      EN
+    </NuxtLink>
+
+    <NuxtLink
+      :to="switchLocalePath('zh')"
+      class="rounded-full border px-3 py-2 text-xs backdrop-blur"
+      :class="
+        locale === 'zh'
+          ? 'bg-secondary/40 text-primary'
+          : 'bg-white text-primary'
+      "
+    >
+      中文
+    </NuxtLink>
+  </div>
+</template>
