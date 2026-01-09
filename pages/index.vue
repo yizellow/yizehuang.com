722e9827c13969bfe1beeb5bdb4a30ebbc1fddc9
yizellow <b10821056@gmcc.tnua.edu.tw>
2026-01-09 22:17:58 +0100

    more to read

--- pages/index.vue
+++ pages/index.vue
@@ -169,7 +169,7 @@ onBeforeUnmount(() => {
             <div
               class="px-10 bg-secondary/60 silkscreen w-3/4 h-[5vh] border-2 rounded-4xl text-primary flex justify-center items-center mb-2 p-2 seemore"
             >
-              MORE...
+              Read...
             </div></a
           >
         </div>
